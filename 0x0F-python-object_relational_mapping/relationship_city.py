3fe03f5e148aa9a6fd6d38a75ba576b5fc308f2b
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2023-03-19 20:17:15 +0100

    Add files via upload

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text, ForeignKey
-from relationship_state import Base
-"""
-    Module that performs creates a States class based off of Base.
-"""
+# Defines a City model.
+# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
+    """Represents a city for a MySQL database.
+
+    Attributes:
+        id (sqlalchemy.Column): The city's id.
+        name (sqlalchemy.Column): The city's name.
+        state_id (sqlalchemy.Column): The city's state id.
     """
-        The ``City`` class which inherits from ``Base`` class.
-    """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
